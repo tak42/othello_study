8b45b5a0b0d03f1988a9274bf4eadd97576956c4
tak42 <1206ookubo@gmail.com>
2021-12-02 20:40:34 +0900

    候補を表示＆パスを追加（２回で終了）

--- pages/stage2_index.tsx
+++ pages/stage2_index.tsx
@@ -36,41 +36,117 @@ const Grid = styled.div`
     width: 100%;
   }
 `
-const Block = styled.div`
+const Block = styled.div<{ val: number }>`
   width: 100px;
   height: 100px;
+  background-color: ${(props) => (props.val === 1 ? 'yellow' : 'green')};
   border: 1px solid white;
 `
 
 const Stone = styled.div<{ val: number }>`
   height: 60px;
   margin: 1rem;
-  background-color: ${(props) => (props.val === 1 ? 'white' : 'black')};
+  background-color: ${(props) => (props.val === 1 ? 'black' : 'white')};
   border-radius: 50%;
 `
 
 type Cell = { row: number; col: number }
 const Home: NextPage = () => {
   // prettier-ignore
+  // const [board, setBoard] = useState([
+  //   [0,0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,0,0],
+  //   [0,0,0,1,2,0,0,0],
+  //   [0,0,0,2,1,0,0,0],
+  //   [0,0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,0,0]
+  // ])
   const [board, setBoard] = useState([
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,1,2,0,0,0],
-    [0,0,0,2,1,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0]
+    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1],
+    [0,0,0,0,2,1,1,1],
   ])
+  const [turnColor, setTurnColor] = useState(1)
+  const [passCount, setPassCount] = useState(0)
+
+  const reverseColor: number = useMemo(() => {
+    return turnColor === 1 ? 2 : 1
+  }, [turnColor])
+
+  const puttables = useMemo(() => {
+    // prettier-ignore
+    const rtnList = [
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0]
+    ]
+    // prettier-ignore
+    const directions: number[][] = [
+      [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]
+    ]
+    const candidates: Cell[] = []
+    // 石を置く候補となる条件
+    // 1.何も置かれていない
+    // 2.８方向のどこかに石がある
+    // 3.石が置かれている方向に２種類の石がある
+    // 4.石を置くと、隣り合う石がひっくり返る
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        if (board[x][y] === 0) {
+          for (const direction of directions) {
+            for (let n = 1; n < 8; n++) {
+              const newX = x + direction[0] * n
+              const newY = y + direction[1] * n
+              if (newX < 0 || newY < 0 || newX > 7 || newY > 7) break
+              if (board[newX][newY] === reverseColor) {
+                candidates.push({ row: newX, col: newY })
+              } else if (board[newX][newY] === turnColor) {
+                candidates.push({ row: newX, col: newY })
+                break
+              } else {
+                break
+              }
+            }
+            if (candidates.length > 1) {
+              const lastCell = candidates[candidates.length - 1]
+              if (board[lastCell.row][lastCell.col] === turnColor) {
+                rtnList[x][y] = 1
+              }
+            }
+            candidates.splice(0, candidates.length)
+          }
+        }
+      }
+    }
+    return rtnList
+  }, [board, turnColor])
+
+  const isPass = useMemo(() => {
+    const OneDimensional = puttables.flat(2)
+    return !OneDimensional.includes(1)
+  }, [puttables])
+
   const { whiteCount, blackCount } = useMemo(() => {
     let blCo = 0 // 黒の石の数
     let whCo = 0 // 白の石の数
     for (let x = 0; x < 8; x++) {
       for (let y = 0; y < 8; y++) {
-        if (board[x][y] === 1) {
+        if (board[x][y] === 2) {
           whCo += 1
         }
-        if (board[x][y] === 2) {
+        if (board[x][y] === 1) {
           blCo += 1
         }
       }
@@ -78,26 +154,47 @@ const Home: NextPage = () => {
     return { whiteCount: whCo, blackCount: blCo }
   }, [board])
   useEffect(() => {
-    console.log(whiteCount + blackCount)
+    const pCnt: number = isPass ? passCount + 1 : 0
+    console.log(pCnt)
+    setPassCount(pCnt)
+    if (isPass && whiteCount + blackCount < 64) {
+      alert('打てるところがないためパスします。')
+      setTurnColor(reverseColor)
+    }
+    if (pCnt > 1) {
+      alert('パスが２回続いたので終了します。')
+      whiteCount > blackCount ? alert('白の勝ちです。') : alert('黒の勝ちです。')
+      resetBoard()
+    }
+  }, [puttables])
+
+  useEffect(() => {
     if (whiteCount + blackCount === 64) {
       if (whiteCount > blackCount) {
         alert('白の勝ちです。')
       } else {
         alert('黒の勝ちです。')
       }
+      resetBoard()
     }
   }, [whiteCount, blackCount])
-  const [turnColor, setTurnColor] = useState(1)
-  // const boardCommit = (newBoard: number[][], reverseColor: number) => {
-  //   return new Promise((resolve, reject) => {
-  //     setBoard(newBoard) // boardに変更を反映
-  //     setTurnColor(reverseColor)
-  //     resolve(reverseColor)
-  //   })
-  // }
+
+  const resetBoard = () => {
+    // prettier-ignore
+    const newBoard: number[][] = [
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,1,2,0,0,0],
+      [0,0,0,2,1,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0]
+    ]
+    setBoard(newBoard)
+  }
   const onClick = (x: number, y: number) => {
     const newBoard: number[][] = JSON.parse(JSON.stringify(board)) // boardを直接書き換えないようにコピー作成
-    const reverseColor: number = turnColor === 1 ? 2 : 1
     // prettier-ignore
     const directions: number[][] = [
       [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]
@@ -132,17 +229,6 @@ const Home: NextPage = () => {
     if (newBoard[x][y] === turnColor) {
       setBoard(newBoard) // boardに変更を反映
       setTurnColor(reverseColor)
-      // homeがレンダリングされた後に判定する必要がある
-      // boardCommit(newBoard, reverseColor).then(() => {
-      // console.log(whiteCount + blackCount)
-      // if (whiteCount + blackCount === 64) {
-      //   if (whiteCount > blackCount) {
-      //     alert('白の勝ちです。')
-      //   } else {
-      //     alert('黒の勝ちです。')
-      //   }
-      // }
-      // })
     }
   }
   return (
@@ -154,11 +240,11 @@ const Home: NextPage = () => {
       </Head>
 
       <Main>
-        <h1>{turnColor === 1 ? '白の番です。' : '黒の番です。'}</h1>
+        <h1>{turnColor === 1 ? '黒の番です。' : '白の番です。'}</h1>
         <Grid>
           {board.map((row, x) =>
             row.map((color, y) => (
-              <Block key={`${x}-${y}`} onClick={() => onClick(x, y)}>
+              <Block key={`${x}-${y}`} onClick={() => onClick(x, y)} val={puttables[x][y]}>
                 {x}, {y}
                 {color > 0 && <Stone val={color} />}
               </Block>
